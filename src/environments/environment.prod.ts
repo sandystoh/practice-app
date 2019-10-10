61ae749caac8040f91def42399ea043ce594e556
Sandy Toh <sandytoh@u.nus.edu>
2019-10-10 21:42:20 +0800

    travis

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: 'http://api.openweathermap.org/data/2.5/weather',
+  api_key: '8cf5fa7f056c9d29e4721bf7fab83ffb'
 };
